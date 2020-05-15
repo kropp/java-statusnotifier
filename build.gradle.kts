eb94c4b7612b832259a497b825b7d61700e13d31
Victor Kropp <victor.kropp@gmail.com>
2020-05-15 15:52:08 +0200

    don't hide DBus exceptions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,18 @@ plugins {
 }
 
 repositories {
+  mavenCentral()
   maven(url = "https://jitpack.io")
 }
 
 group = "com.github.kropp"
-version = "0.1"
-
-repositories {
-  mavenCentral()
-}
+version = "0.1.4"
 
 dependencies {
   implementation("com.github.hypfvieh:dbus-java:master")
+  val slf4jVersion = "1.7.26"
+  implementation("org.slf4j:slf4j-api:$slf4jVersion")
+  implementation("org.slf4j:slf4j-simple:$slf4jVersion")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
 }
@@ -27,5 +27,5 @@ configure<JavaPluginConvention> {
 tasks.wrapper {
   distributionType = Wrapper.DistributionType.BIN
   gradleVersion = "6.3"
-  distributionUrl = "https://cache-redirector.jetbrains.com/services.gradle.org/distributions/gradle-$gradleVersion-bin.zip"
+  distributionUrl = "https://services.gradle.org/distributions/gradle-$gradleVersion-bin.zip"
 }
--- src/main/java/org/freedesktop/StatusNotifierItem.java
+++ src/main/java/org/freedesktop/StatusNotifierItem.java
@@ -6,17 +6,20 @@ import org.freedesktop.dbus.exceptions.DBusException;
 import java.util.List;
 
 public class StatusNotifierItem {
-  public StatusNotifierItem(String serviceName, String title, IStatusNotifierItem.Category category, List<Pixmap> pixmaps, List<MenuItem> menuItems) {
+  public StatusNotifierItem(String serviceName, String title, IStatusNotifierItem.Category category, List<Pixmap> pixmaps, List<MenuItem> menuItems) throws DBusException {
     try {
       statusNotifierItem = new StatusNotifierItemImpl(serviceName, title, category, pixmaps);
       new DBusMenu(serviceName, menuItems);
     } catch (DBusException e) {
       statusNotifierItem = null;
+      throw e;
     }
   }
 
   public void onActivate(StatusNotifierListener listener) {
-    statusNotifierItem.addActivateListener(listener);
+    if (statusNotifierItem != null) {
+      statusNotifierItem.addActivateListener(listener);
+    }
   }
 
   public static class MenuItem {
